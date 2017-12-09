6f88d7d931156d281d9ee8189f9afa45ea5ffa5a
Administrador <Administrador@DavidCornejo>
2017-12-09 16:32:37 -0500

    Creacion e implementacion de eventos de cada ventana

---
+++ Practica02_02/src/controlador/EventoVentanaPrincipal.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package controlador;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import vista.VentanaCurso;
+import vista.VentanaDocente;
+import vista.VentanaMateria;
+import vista.VentanaPrincipal;
+
+/**
+ *
+ * @author Administrador
+ */
+public class EventoVentanaPrincipal implements ActionListener{
+    
+    private VentanaPrincipal vPrincipal;
+
+    public EventoVentanaPrincipal(VentanaPrincipal vPrincipal) {
+        this.vPrincipal = vPrincipal;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        
+        if(e.getSource().equals(this.vPrincipal.getItemMenuList().get(0))){
+            this.vPrincipal.setvCurso(new VentanaCurso("Nuevo Curso",true, true,true,true,this.vPrincipal.getGd()));
+            this.vPrincipal.getEscritorio().add(this.vPrincipal.getvCurso());
+        }
+        
+        if(e.getSource().equals(this.vPrincipal.getItemMenuList().get(1))){
+            this.vPrincipal.setvDocente(new VentanaDocente("Nuevo Docente",true, true,true,true, this.vPrincipal.getGd()));
+            this.vPrincipal.getEscritorio().add(this.vPrincipal.getvDocente());
+        }
+        
+        if(e.getSource().equals(this.vPrincipal.getItemMenuList().get(2))){
+            this.vPrincipal.setvMateria(new VentanaMateria("Nueva Materia",true, true,true,true, this.vPrincipal.getGd()));
+            this.vPrincipal.getEscritorio().add(this.vPrincipal.getvMateria());
+        }
+        
+        
+    }
+    
+}
